adc1d45c367e7c86b22ee1596cc25c00966b2568
grant2258 <grant2258@example.com>
2021-01-09 18:17:14 +0000

    fix gfx up

--- src/vidhrdw/genesis.c
+++ src/vidhrdw/genesis.c
@@ -31,10 +31,10 @@
 #define VDP_VRAM_WORD(x)	((VDP_VRAM_BYTE(x) << 8) | VDP_VRAM_BYTE((x) + 1))
 #define VDP_VSRAM_WORD(x)	((VDP_VSRAM_BYTE(x) << 8) | VDP_VSRAM_BYTE((x) + 1))
 
-#ifdef LSB_FIRST
-#define EXTRACT_PIXEL(x,i)	(((x) >> (((i) ^ 1) * 4)) & 0x0f)
-#else
+#ifdef MSB_FIRST
 #define EXTRACT_PIXEL(x,i)	(((x) >> (((i) ^ 7) * 4)) & 0x0f)
+#else
+#define EXTRACT_PIXEL(x,i)	(((x) >> (((i) ^ 1) * 4)) & 0x0f)
 #endif
 
 
